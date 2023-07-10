08aedc8a8cff100a0961d0624e9a405935d322e5
bdhayes01 <briandhayes01@gmail.com>
2023-07-10 14:51:30 -0600

    Reorganized the functions

--- image_inspector.py
+++ image_inspector.py
@@ -21,6 +21,7 @@ Created on Wed Feb 10 15:58:09 2021
 #       a. Make examples for this in colab that can be shown to JC. Screenshot them!
 # 9. Put the documents onto the lab website online.
 # 10. Ask if the mass up/ down should add 1 or the m/z spacing?
+# 11. Put a form for reporting errors
 
 import os
 import sys
